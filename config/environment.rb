8335b7b9dc0f8b55d2e7468cae47555ced6a66a7
Michael Maranda <tropology@gmail.com>
2011-03-27 22:34:18 -0500

    gem reg changed to r 3.05 -- everything wil be broken initially

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ require File.join(File.dirname(__FILE__), 'wagn_initializer')
   
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
-  #RAILS_GEM_VERSION = '3.0.4' unless defined? RAILS_GEM_VERSION  
+  #RAILS_GEM_VERSION = '3.0.5' unless defined? RAILS_GEM_VERSION  
   
   Wagn::Initializer.set_default_rails_config config
 
